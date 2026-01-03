062f38ee87c2574e5797ad172eefa8fb01dfeed7
Courtney Johnson <cjandy13@gmail.com>
2026-01-02 20:51:46 -0600

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+      <div id='topBar'> 
+        <h1 id='tbd'>SHOELADY</h1>
+
+        <p className='menu'>Gallery</p>&nbsp;
+        <p className='menu'>About</p>&nbsp;
+        <p className='menu'>Contact</p>&nbsp;
+        <p className='menu'>Shop</p>&nbsp;
+      </div>
+
+      <section id='att'>
+        <h2 id='blah'>ATTENTION</h2>&nbsp;
+        <h2>BOX</h2>
+      </section>
+
+    </section>
+  )
+
 }
 
 export default App;
